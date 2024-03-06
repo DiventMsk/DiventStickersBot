fbd3a3719e704db64384508850bb9cdd3ed8d3b2
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-03-06 23:25:12 +0500

    kv

--- api/image.mjs
+++ api/image.mjs
@@ -1,15 +1,18 @@
 import { bot } from '../src/bot.mjs'
 import { InputFile } from 'grammy'
+import { kv } from '@vercel/kv'
 
 const { DEFAULT_USER_ID } = process.env
 export const POST = async req => {
   await bot.init()
+  const id = /** @type string */ req.headers['x-vercel-id'].split(':').pop()
   const { file_id } = await bot.api.uploadStickerFile(
     parseInt(DEFAULT_USER_ID),
     'static',
     new InputFile(req.body)
   )
+  await kv.set(id, file_id)
   const url = new URL(bot.botInfo.username, 'https://t.me')
-  url.searchParams.set('start', file_id)
+  url.searchParams.set('start', id)
   return Response.json({ url })
 }
