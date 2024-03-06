42055ae4278ef492f31af37be3d57c12d901ebb4
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-03-06 23:42:01 +0500

    fix

--- src/bot.mjs
+++ src/bot.mjs
@@ -15,6 +15,27 @@ export const bot = new Bot(token)
 const safe = bot.errorBoundary(console.error)
 
 safe.command('start', async ctx => {
-  const id = /** @type string */ await kv.get(ctx.match)
-  await ctx.replyWithSticker(id)
+  const sticker = /** @type string */ await kv.get(ctx.match)
+  const name = `${ctx.chat.id}by_${ctx.me.username}`
+  try {
+    await ctx.api.addStickerToSet(ctx.chat.id, name, {
+      sticker,
+      emoji_list: ['✨'],
+    })
+  } catch {
+    await ctx.api.createNewStickerSet(
+      ctx.chat.id,
+      name,
+      '@DiventDigital',
+      [
+        {
+          sticker,
+          emoji_list: ['✨'],
+        },
+      ],
+      'static'
+    )
+  } finally {
+    await ctx.reply(`t.me/addstickers/${name}`)
+  }
 })
