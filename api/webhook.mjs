048f918af90dbdb643f0be01a45d067be56b7e4a
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-03-07 00:56:59 +0500

    update

--- api/webhook.mjs
+++ api/webhook.mjs
@@ -1,9 +1,9 @@
-import {bot, secretToken} from "../src/bot.mjs";
-import {setWebhookCallback} from "vercel-grammy";
+import { bot, secretToken } from '../src/bot.mjs'
+import { setWebhookCallback } from 'vercel-grammy'
 
 // Handler to set webhook url based on request headers
 export const POST = setWebhookCallback(bot, {
-    path: "api/update",
-    onError: "return",
-    secretToken
-});
+  path: 'api/update',
+  onError: 'return',
+  secretToken,
+})
--- index.mjs
+++ index.mjs
@@ -1,13 +1,14 @@
 import meow from 'meow'
 import { readFileSync } from 'node:fs'
 
-const { IMAGE_API_URL } = process.env
-
 const {
   input: [path],
 } = meow({ importMeta: import.meta })
 const body = readFileSync(path)
-const response = await fetch(IMAGE_API_URL, { method: 'POST', body })
+const response = await fetch(
+  'https://divent-stickers-bot.vercel.app/api/image',
+  { method: 'POST', body }
+)
 const { url } = await response.json()
 
 console.log(url)
