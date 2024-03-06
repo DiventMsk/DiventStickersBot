21a0d6388270ba60bd266b7246ac7f4747c938a4
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-03-06 21:47:33 +0500

    update

--- api/image.mjs
+++ api/image.mjs
@@ -1,3 +1,8 @@
-export const POST = req => {
-  return Response.json({ id: '123' })
+import { bot } from '../src/bot.mjs'
+
+export const POST = async req => {
+  await bot.init()
+  const url = new URL(bot.botInfo.username, 'https://t.me')
+  url.searchParams.set('start', '12345')
+  return Response.json({ url })
 }
