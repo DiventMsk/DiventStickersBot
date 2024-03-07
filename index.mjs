dac53e20447ddac37d263374ad6c4f9fb569862f
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-03-07 21:51:27 +0500

    Update

--- index.mjs
+++ index.mjs
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
 import meow from 'meow'
-import { readFileSync } from 'node:fs'
+import { createReadStream } from 'node:fs'
 
 const {
   input: [path],
 } = meow({ importMeta: import.meta })
-const body = readFileSync(path)
-const response = await fetch(
-  'https://divent-stickers-bot.vercel.app/api/image',
-  { method: 'POST', body }
-)
+const stream = createReadStream(path)
+const body = ReadableStream.from(stream.iterator())
+const apiURL = 'https://divent-stickers-bot.vercel.app/api/image'
+const response = await fetch(apiURL, { method: 'POST', body, duplex: 'half' })
 const { url } = await response.json()
 
 console.log(url)
