42f668bb7607b5dad160159431956cf1c68aaee7
DiventMsk <diventtech@gmail.com>
2024-09-06 00:27:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Run script by:
+
+```shell
+node . --sex male path/to/image.webp
+```
+
+or
+
+```shell
+node path/to/index.mjs path/to/image.webp .../image2.webp .../image3.webp
+```
---
+++ index.mjs
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import meow from 'meow'
+import {userInfo} from 'node:os'
+import {createReadStream} from 'node:fs'
+
+const {
+    API_URL = 'https://divent-stickers.vercel.app/',
+    CLIENT_ID = userInfo().username.trim().replaceAll(' ', '-'),
+} = process.env
+
+const {input = [], flags: {sex} = {}} = meow({
+    importMeta: import.meta,
+    flags: {
+        sex: {
+            type: 'string',
+            shortFlag: 's',
+            default: 'male',
+            isRequired: true,
+            choices: ['male', 'female'],
+        },
+    },
+})
+
+const api = {
+    client: new URL(`api/v1/client/${CLIENT_ID}`, API_URL),
+    image: new URL(`api/v1/image/${CLIENT_ID}`, API_URL),
+    group: new URL(`api/v1/group/${CLIENT_ID}`, API_URL),
+}
+
+const requestPromise = fetch(api.client)
+
+if (!input.length) {
+    console.log(api.client.href)
+    console.log(await requestPromise.then(r => r.json()))
+    process.exit()
+}
+
+const images = await Promise.all(
+    input.map(async path => {
+        try {
+            const stream = createReadStream(path)
+            const body = ReadableStream.from(stream.iterator())
+            const response = await fetch(api.image, {
+                method: 'POST',
+                duplex: 'half',
+                body,
+            })
+            const {file_id} = await response.json()
+            return file_id
+        } catch {
+        }
+    })
+)
+
+const response = await fetch(api.group, {
+    body: JSON.stringify({sex, stickers: images.filter(Boolean)}),
+    method: 'POST',
+})
+
+const {url} = await response.json()
+
+console.log(url)
